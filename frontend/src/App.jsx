95248ce35ba02b76e10422a5c9e5f957a636b676
ymiu0001 <ymiu0001@student.monash.edu>
2025-08-05 23:04:17 +0900

    "resolve conflict"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,10 @@ import RequestPage from './components/victim/RequestPage';
 import ConfirmPage from './components/victim/ConfirmPage';
 import CompletePage from './components/victim/CompletePage';
 import StatusPage from './components/victim/StatusPage';
-import LoginPage from './components/Supporter/LoginPage.jsx'
+import SupporterLoginPage from './components/Supporter/LoginPage.jsx'
 import SupporterReq from './components/Supporter/SupporterReq.jsx'
 import SupporterConfirm from './components/Supporter/SupporterConfirm.jsx'
-import CompletePage from './components/Supporter/CompletePage.jsx'
+import SupporterCompletePage from './components/Supporter/CompletePage.jsx'
 
 
 function App() {
@@ -28,12 +28,12 @@ function App() {
         <Route path="complete" element={<CompletePage />} />
         <Route path="status" element={<StatusPage />} />
       </Route>
-      <Routes path="/supporter">
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/request" element={<SupporterReq />} />
-        <Route path="/confirm" element={<SupporterConfirm />} />
-        <Route path="/status" element={<CompletePage />} />
-      </Routes>
+      <Route path="/supporter">
+        <Route path="login" element={<SupporterLoginPage />} />
+        <Route path="request" element={<SupporterReq />} />
+        <Route path="confirm" element={<SupporterConfirm />} />
+        <Route path="status" element={<SupporterCompletePage />} />
+      </Route>
     </Routes>
   );
 }
