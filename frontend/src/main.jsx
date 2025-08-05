dfbc055acc6eb56fc2729402fa3c36091219fdd1
Shinjiro Tsukuda (佃 真次郎) <tsukushin_volley62@icloud.com>
2025-08-05 16:58:47 +0900

    feat: 被災者側の画面遷移の枠組み作成

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
 // src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ★importが重要
+import './index.css';
+import App from './App.jsx'; // ★Appコンポーネントを読み込む
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* ★Appコンポーネント全体を<BrowserRouter>で囲む */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
