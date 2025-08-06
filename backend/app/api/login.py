a0c5e5d4e67c4fa811a9b456f71c902088be6e00
Matsumoto_Hirotada <matsumoto_hirotada@live2d.com>
2025-08-06 13:42:10 +0900

    ログイン機能のDBと通信する部分を追加

--- backend/app/api/login.py
+++ backend/app/api/login.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+import sqlite3
 
 router = APIRouter()
 
@@ -10,12 +11,29 @@ class LoginRequest(BaseModel):
 @router.post("/login")
 def login(request: LoginRequest):
 
+    #DBへの問い合わせ処理実装
+    conn = sqlite3.connect('database/app.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT password FROM user WHERE email = ?', (request.username_or_email, ))
+    password = cursor.fetchall()
+
+    if not password:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    else:
+        print(list(password[0])[0])
+        if request.password == list(password[0])[0]:
+            return {"success": True, "message": "Login successful"}
+        else:
+            raise HTTPException(status_code=401, detail="Invalid email or password")
+
+"""
     # Hardcoded check for demo
     if request.username_or_email == "hanako@example.com" and request.password == "cde123":
         return {"success": True, "message": "Login successful"}
 
     # If it doesn't match
     raise HTTPException(status_code=401, detail="Invalid email or password")
+"""
 
     # db = SessionLocal()
     # # Search user by username or email
